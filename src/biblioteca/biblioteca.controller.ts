0dc274c6f21c147991ea373dd3bddff4db9d55a7
dpareja_meli <david.pareja@mercadolibre.com.co>
2025-05-15 17:36:45 -0500

    crear interceptador de errores

--- src/biblioteca/biblioteca.controller.ts
+++ src/biblioteca/biblioteca.controller.ts
@@ -1,7 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { BibliotecaService } from './biblioteca.service';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors.interceptor';
 
 @Controller('bibliotecas')
+@UseInterceptors(BusinessErrorsInterceptor)
 export class BibliotecaController {
     constructor(private readonly museumService: BibliotecaService) {}
 
