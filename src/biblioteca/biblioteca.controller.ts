f65f4c6af9cd8fc1e7639542af3da9109ed20329
dparejaUniandes <d.pareja@uniandes.edu.co>
2025-05-15 21:05:49 -0500

    finalizar controlador biblioteca

--- src/biblioteca/biblioteca.controller.ts
+++ src/biblioteca/biblioteca.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
 import { BibliotecaService } from './biblioteca.service';
 import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors.interceptor';
 import { plainToInstance } from 'class-transformer';
@@ -9,19 +9,34 @@ import { ActualizarBibliotecaDto } from './actualizar-biblioteca.dto';
 @Controller('bibliotecas')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class BibliotecaController {
-    constructor(private readonly museumService: BibliotecaService) {}
+    constructor(private readonly bibliotecaService: BibliotecaService) {}
     
+    @Get()
+    async findAll() {
+        return await this.bibliotecaService.findAll();
+    }
+
+    @Get(':bibliotecaId')
+    async findOne(@Param('bibliotecaId') bibliotecaId: string) {
+        return await this.bibliotecaService.findOne(bibliotecaId);
+    }
 
     @Post()
     async create(@Body() bibliotecaDto: CrearBibliotecaDto) {
         const biblioteca: BibliotecaEntity = plainToInstance(BibliotecaEntity, bibliotecaDto);
-        return await this.museumService.create(biblioteca);
+        return await this.bibliotecaService.create(biblioteca);
     }
 
     @Put(':bibliotecaId')
     async update(@Param('bibliotecaId') bibliotecaId: string, @Body() bibliotecaDto: ActualizarBibliotecaDto) {
         const biblioteca: BibliotecaEntity = plainToInstance(BibliotecaEntity, bibliotecaDto);
-        return await this.museumService.update(bibliotecaId, biblioteca);
+        return await this.bibliotecaService.update(bibliotecaId, biblioteca);
+    }
+
+    @Delete(':bibliotecaId')
+    @HttpCode(204)
+    async delete(@Param('bibliotecaId') bibliotecaId: string) {
+        return await this.bibliotecaService.delete(bibliotecaId);
     }
 
 }
